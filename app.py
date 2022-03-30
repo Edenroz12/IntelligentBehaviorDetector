36908374d44b23b43f8693cde3b923661397c73e
Edenroz12 <93472881+Edenroz12@users.noreply.github.com>
2022-03-30 20:02:54 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+import DB_manager
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():  # put application's code here
-    return render_template('sign_in.html')
-@app.route()
+@app.route('/', methods=['POST', 'GET'])
 def sign_in():
-    return None
+    if request.method == 'GET':
+        return render_template('sign_in.html')
+    else:  # request.method == 'POST'
+        user_email = request.form['email']
+        if DB_manager.sign_in(user_email):
+            return render_template('homepage.html')
+        else:
+            return render_template('sign_in.html')
+
 
 @app.route('/sign_up')
 def sign_up():
